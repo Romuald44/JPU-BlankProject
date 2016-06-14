21a6c2f8ee4b6895978c2a370462f60d7bcd50e1
AurelienMoureau <admmoureau@gmail.com>
2016-06-14 16:05:34 +0200

    Model.Element

---
+++ model/src/main/java/model/element/Sprite.java
@@ -0,0 +1,23 @@
+package model.element;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Sprite {
+	private Image picture;
+
+	public Sprite(final String picture) {
+		try {
+			this.picture = ImageIO.read(new File("images/" + picture));
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Image getImage() {
+		return this.picture;
+	}
+}

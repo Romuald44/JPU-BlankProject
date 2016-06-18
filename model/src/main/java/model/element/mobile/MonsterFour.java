5087eafcf3b6f8cf2c6f15638fb39bf59b4e458e
Romuald44 <romuald.lebrun44@gmail.com>
2016-06-18 18:14:08 +0200

    Try movement monster

--- model/src/main/java/model/element/mobile/MonsterFour.java
+++ model/src/main/java/model/element/mobile/MonsterFour.java
@@ -1,13 +1,18 @@
 package model.element.mobile;
 
+import java.awt.Point;
+import java.util.Random;
+
 import model.Model;
 import model.element.Sprite;
 import model.element.motionless.Gate;
+import model.element.motionless.Land;
 import model.element.motionless.Purse;
 
 public class MonsterFour implements IBehavior {
 	
 	private Sprite sprite;
+	private Point direction = new Point(1,1);
 
 	/* Monstre qui ne se déplace pas en diagonale */
 	public MonsterFour() {
@@ -15,14 +20,43 @@ public class MonsterFour implements IBehavior {
 	}
 	
 	public void movement(Lorann lorann, Demons demons, Model model) {
-		if(demons.isMovePossible((int) demons.getPosition().getX()+1, (int) demons.getPosition().getY())) {
-			if((model.getElements((int) demons.getPosition().getX()+1, (int) demons.getPosition().getY()) instanceof Purse) ||
-			(model.getElements((int) demons.getPosition().getX()+1, (int) demons.getPosition().getY()) instanceof Gate)	) {
+		Random rand = new Random();
+		int nombreAleatoire = rand.nextInt(4);
+		System.out.println(nombreAleatoire);
+		
+		if(nombreAleatoire == 1) {
+			direction.x *= 1;
+		}
+		else if(nombreAleatoire == 2) {
+			direction.x *= -1;
+		}
+		else if(nombreAleatoire == 3) {
+			direction.y *= 1;
+		}
+		else if(nombreAleatoire == 4) {
+			direction.y *= -1;
+		}
+		if(demons.isMovePossible((int) demons.getPosition().getX(), (int) demons.getPosition().getY())) {
+			if(direction.x == 1) {
+				demons.getPosition().x++;
+			}
+			else if(direction.x == -1) {
 				demons.getPosition().x--;
+			}
+			else if(direction.y == 1) {
+				demons.getPosition().y++;
+			}
+			else if(direction.y == -1) {
+				demons.getPosition().y--;
+			}
+			/*if((model.getElements((int) demons.getPosition().getX()+1, (int) demons.getPosition().getY()) instanceof Land) ||
+			(model.getElements((int) demons.getPosition().getX()+1, (int) demons.getPosition().getY()) instanceof Gate)	) {
+				demons.getPosition().x += direction.x;
+				demons.getPosition().y += direction.y;
 			}
 			else {
 				demons.getPosition().x++;
-			}
+			}*/
 		}
 	}
 

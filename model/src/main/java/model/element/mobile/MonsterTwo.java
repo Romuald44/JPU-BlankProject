2af2822bbdc7fbbe4cc71aa1856c56be0b6bef70
Romuald44 <romuald.lebrun44@gmail.com>
2016-06-19 14:21:21 +0200

    Monster Two random movement

--- model/src/main/java/model/element/mobile/MonsterTwo.java
+++ model/src/main/java/model/element/mobile/MonsterTwo.java
@@ -9,14 +9,45 @@ public class MonsterTwo implements IBehavior {
 	
 	private Sprite sprite;
 
-	/* Monstre déplacement random */
+	/* Monster random movement */
 	public MonsterTwo() {
 		this.sprite = new Sprite("monster_2.png");
 	}
 
 	public Point movement(Lorann lorann, Point demons, Model model) {
-		// TODO Auto-generated method stub
-		return null;
+		int rand = (int)(Math.random() * 8) + 1; 
+		switch(rand){
+		case 1:
+			demons.x = demons.x + 1;
+			return demons;
+		case 2:
+			demons.x = demons.x - 1;
+			return demons;
+		case 3:
+			demons.y = demons.y + 1;
+			return demons;
+		case 4:
+			demons.y = demons.y - 1;
+			return demons;
+		case 5:
+			demons.x = demons.x + 1;
+			demons.y = demons.y + 1;
+			return demons;
+		case 6:
+			demons.x = demons.x - 1;
+			demons.y = demons.y + 1;
+			return demons;
+		case 7:
+			demons.x = demons.x + 1;
+			demons.y = demons.y - 1;
+			return demons;
+		case 8:
+			demons.x = demons.x - 1;
+			demons.y = demons.y - 1;
+			return demons;
+		default :
+			return demons;
+		}
 	}
 
 	public Sprite getSprite() {
